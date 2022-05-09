27da417ea9f33553535b95471644d05bd9fe1e0f
Tempest <Misha8936@yandex.ru>
2022-05-09 18:12:37 +0500

    refactor: generalize db get by field methods

--- app_code/controller/db/mongo.py
+++ app_code/controller/db/mongo.py
@@ -1,13 +1,15 @@
 import os
 from os.path import join, dirname
+from typing import List, Union, Type
+
+from dotenv import load_dotenv
+from orjson import orjson
+from pydantic import BaseModel
 from pymongo import MongoClient
 from pymongo.errors import OperationFailure
 from pymongo.results import DeleteResult, InsertOneResult, UpdateResult, InsertManyResult
-from orjson import orjson
+
 from app_code.models.entities import Order, Driver, Solution, Customer
-from typing import List, Union
-from dotenv import load_dotenv
-from pydantic import BaseModel
 
 dotenv_path = join(dirname(__file__), 'envs.env')
 load_dotenv(dotenv_path)
@@ -19,7 +21,7 @@ class CustomerExistsException(Exception):
 
 class MongoBase:
     client = MongoClient('mongodb://%s:%s@localhost:27017/' % (os.environ.get('MONGO_USER'),
-                         os.environ.get('MONGO_PASSWORD')))
+                                                               os.environ.get('MONGO_PASSWORD')))
 
     DATABASE_NAME = 'barathrum'
 
@@ -42,17 +44,24 @@ class MongoBase:
             raise e
         return result
 
+    def get_query_result_by_field(self, entity: Type[Union[Order, Driver, Solution, Customer]], field: str,
+                                  value: Union[str, float, int]) -> dict:
+        try:
+            result = self.client[self.DATABASE_NAME][entity.__name__.lower()].find_one({field: f"{value}"})
+        except Exception as e:
+            raise e
+        return result
+
     def upload_order(self, order: Order) -> InsertOneResult:
         return self.upload_entity(order)
 
     def delete_order(self, order: Order) -> DeleteResult:
         return self.delete_entity(order)
 
-    def get_order_by_id(self, order_id: str) -> Order:
-        try:
-            result = self.client[self.DATABASE_NAME]['order'].find_one({'id': order_id})
-        except OperationFailure as e:
-            raise e
+    def get_order_by_id(self, order_id: str) -> Union[Order, None]:
+        result = self.get_query_result_by_field(Order, 'id', order_id)
+        if result is None:
+            return result
         return Order(**result)
 
     def upload_customer(self, customer: Customer) -> InsertOneResult:
@@ -61,10 +70,7 @@ class MongoBase:
         return self.upload_entity(customer)
 
     def get_customer_by_email(self, email: str) -> Union[Customer, None]:
-        try:
-            result = self.client[self.DATABASE_NAME]['customer'].find_one({'email': email})
-        except OperationFailure as e:
-            raise e
+        result = self.get_query_result_by_field(Customer, 'email', email)
         if result is None:
             return None
         return Customer(**result)
@@ -82,10 +88,7 @@ class MongoBase:
         return self.delete_entity(customer)
 
     def get_customer_by_id(self, user_id: str):
-        try:
-            result = self.client[self.DATABASE_NAME]['customer'].find_one({'id': user_id})
-        except OperationFailure as e:
-            raise e
+        result = self.get_query_result_by_field(Customer, 'id', user_id)
         if result is None:
             return None
         return Customer(**result)
