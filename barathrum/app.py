52ad511e3d2b609d1093e86529ae9e68e6d4d8d1
Tempestmon <misha8936@yandex.ru>
2022-07-24 22:44:06 +0500

    fix: app run params

--- barathrum/app.py
+++ barathrum/app.py
@@ -142,9 +142,5 @@ def logout():
     return redirect(url_for("root"))
 
 
-def run():
-    app.run(debug=True)
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
